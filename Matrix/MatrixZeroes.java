1310a19238d1f5499b63191a5b25374491b65a3c
SRIJA MAZUMDER <144462715+Srija-Mazumder@users.noreply.github.com>
2024-12-27 21:43:01 +0530

    Create MatrixZeroes.java

---
+++ Matrix/MatrixZeroes.java
@@ -0,0 +1,46 @@
+public class MatrixZeroes {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 0, 7},
+            {0,  3, 9},
+            {8,  5, 2}
+        };
+
+        setZeroes(matrix);
+
+        // Print the modified matrix
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void setZeroes(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        boolean[] zeroRows = new boolean[rows];
+        boolean[] zeroCols = new boolean[cols];
+
+        // First pass: Identify rows and columns to be zeroed
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    zeroRows[i] = true;
+                    zeroCols[j] = true;
+                }
+            }
+        }
+
+        // Second pass: Set the marked rows and columns to zero
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (zeroRows[i] || zeroCols[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
